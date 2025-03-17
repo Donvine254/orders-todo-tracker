4a2a9637d6cfeb312a6528353c0b97ffbe2ddc59
donvine <donvinemugendi@gmail.com>
2025-03-17 23:47:31 +0300

    Remove database connection string from index.ts and comment out seed data insertion in seed.ts

--- app/db/seed.ts
+++ app/db/seed.ts
@@ -1,17 +1,9 @@
-import { db } from ".";
-import { OrderTable } from "./schema";
+// import { db } from ".";
+// import { OrderTable } from "./schema";
 import "dotenv/config";
 export async function main() {
   const now = new Date();
-  await db.insert(OrderTable).values({
-    orderNumber: "7227442",
-    pages: 3,
-    dueDate: new Date(now.getTime() + 3 * 24 * 60 * 60 * 1000),
-    priority: "low",
-    assignedTo: "Jecinta",
-    note: "Submitted",
-    completed: true,
-  });
+  console.log(now);
 }
 
 main();
