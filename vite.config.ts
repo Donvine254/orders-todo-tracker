fc91250d33265600d20bbbed5aba4a66f8f17432
donvine <donvinemugendi@gmail.com>
2025-03-23 01:10:43 +0300

    Remove netlifyPlugin from Vite configuration and update Netlify dependencies in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 declare module "@remix-run/node" {
   interface Future {
     v3_singleFetch: true;
@@ -20,6 +19,5 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
-    netlifyPlugin(),
   ],
 });
