6bcc8872d675fd1c19cbf1670a88427bfead133b
donvine <donvinemugendi@gmail.com>
2025-03-24 01:24:44 +0300

    Refactor database connection to use import.meta.env for DATABASE_URL and remove unused code

--- app/db/index.server.ts
+++ app/db/index.server.ts
@@ -2,11 +2,6 @@ import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 dotenv.config();
-// eslint-disable-next-line
-declare const Netlify: any;
-const DATABASE_URL =
-  process.env.MODE === "development"
-    ? process.env.DEV_DATABASE_URL
-    : Netlify.env.get("DATABASE_URL");
-const sql = neon(DATABASE_URL!);
+
+const sql = neon(import.meta.env.DATABASE_URL!);
 export const db = drizzle({ client: sql });
