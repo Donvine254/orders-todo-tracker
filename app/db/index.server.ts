7fc19dced9713b5082f070e08523fc1181c4a729
donvine <donvinemugendi@gmail.com>
2025-03-24 01:22:47 +0300

    Refactor database connection file to use dotenv for environment variables and clean up ESLint comments

--- app/db/index.server.ts
+++ app/db/index.server.ts
@@ -1,8 +1,8 @@
-/*eslint-disable-next-line @typescript-eslint/no-unused-vars*/
 import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 dotenv.config();
+// eslint-disable-next-line
 declare const Netlify: any;
 const DATABASE_URL =
   process.env.MODE === "development"
