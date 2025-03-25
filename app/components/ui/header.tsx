f123ce280282f9e8c20e5dc5fc8d1b564602d39f
donvine <donvinemugendi@gmail.com>
2025-03-25 09:55:16 +0300

    Add dark mode logo and update header component for improved theme support

--- app/components/ui/header.tsx
+++ app/components/ui/header.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Clipboard, Settings } from "lucide-react";
+import { LogOutIcon, Settings } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Button } from "~/components/ui/button";
@@ -43,31 +43,35 @@ export default function Header() {
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="glassmorphism rounded-lg p-4 md:p-6 flex flex-col md:flex-row justify-between items-center gap-4 shadow mb-6 border ">
-      <div className="flex items-center gap-4">
-        <div className="text-todo-primary bg-todo-light p-3 rounded-full">
-          <Clipboard className="h-8 w-8" />
-        </div>
-        <div>
-          <h1 className="text-3xl md:text-4xl font-light text-todo-primary tracking-tight dark:text-todo-light ">
-            Orders Todo list
-          </h1>
-        </div>
-      </div>
-
+      className="glassmorphism  rounded-lg p-4 md:p-6 flex flex-col md:flex-row justify-between items-center gap-4 shadow mb-6 border">
+      <img
+        src="/public/logo.png"
+        style={{ maxWidth: "120px", height: "auto" }}
+        className="dark:hidden"
+        alt="logo"
+        fetchPriority="high"
+      />
+      {/* add a logo for dark theme */}
+      <img
+        src="/public/logo-dark.png"
+        className="hidden dark:block"
+        style={{ maxWidth: "120px", height: "auto" }}
+        alt="logo"
+        fetchPriority="high"
+      />
       <div className="flex flex-col md:flex-row items-center gap-4">
         <p className="text-lg font-medium">{formatDate(currentDate)}</p>
         <div className="flex items-center gap-2">
           <Button variant="secondary" className=" ">
             <a href="/setup" className="flex items-center gap-2">
-              <Settings className="h-4 w-4 mr-2" />
+              <Settings className="h-4 w-4" />
               Setup
             </a>
           </Button>
           <Button
-            variant="destructive"
-            className=" "
+            className="hover:bg-destructive hover:text-white "
             onClick={() => handleLogout()}>
+            <LogOutIcon className="h-4 w-4" />
             Logout
           </Button>
         </div>
